25180d48fbc9318a552d7e57adc49ac51a61f7d4
Belinda Slakman <slakman.b@husky.neu.edu>
2014-06-18 16:55:45 -0400

    Removed some reaction families.
    As per Fariba's suggestion, remove families making weird 
    things so that we build a simpler model faster.

--- input.py
+++ input.py
@@ -4,7 +4,7 @@ database(
     reactionLibraries = [],
     seedMechanisms = [],
     kineticsDepositories = ['training'], #  'all', 'default'==['training'], [], 
-    kineticsFamilies = ['!Intra_Disproportionation','!Substitution_O', '!Diels_alder_addition'],
+    kineticsFamilies = ['!Intra_Disproportionation','!Substitution_O', '!Diels_alder_addition', '!1,4_Cyclic_birad_scission', '!1,4_Linear_birad_scission'],
     kineticsEstimator = 'rate rules',
 )
 
